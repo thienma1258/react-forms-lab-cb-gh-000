a7bf6cb4f13c838c119aa824e48a6314e2bd71a0
Ngoc Dong Pham <cpud1258@gmail.com>
2018-10-01 13:03:36 +0000

    automatically backed up by learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -10,7 +10,7 @@ message:''
     };
   }
   handleInputChange=(event)=>{
-    this.setState({   [event.target.name]: event.target.value})
+    this.setState({   event.target.name: event.target.value})
   }
   render() {
     return (
